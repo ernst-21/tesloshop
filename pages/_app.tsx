953d5d310d7b0a0675c58daae7040baecdf3d88e
riverag8504 <riverag8504@gmail.com>
2022-06-16 22:27:27 -0400

    created swrFetch

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,22 @@
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import type { AppProps } from "next/app";
-import { lightTheme } from "../themes";
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import type { AppProps } from 'next/app';
+import { SWRConfig } from 'swr';
+import { lightTheme } from '../themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider theme={lightTheme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+	return (
+		<SWRConfig
+			value={{
+				fetcher: (resource, init) =>
+					fetch(resource, init).then((res) => res.json()),
+			}}
+		>
+			<ThemeProvider theme={lightTheme}>
+				<CssBaseline />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</SWRConfig>
+	);
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,27 @@
-import { Typography } from "@mui/material";
-import type { NextPage } from "next";
-import { ShopLayout } from "../components/layouts";
-import { ProductList } from "../components/products";
-import { initialData } from "../database/products";
+import { Typography } from '@mui/material';
+import type { NextPage } from 'next';
+import { ShopLayout } from '../components/layouts';
+import { ProductList } from '../components/products';
+import { useProducts } from '../hooks';
+import { FullScreenLoading } from '../components/ui';
 
-const Home: NextPage = () => {
-  return (
-    <ShopLayout
-      title={"Teslo-Shop - Home"}
-      pageDescription={"Encuentra Productos"}
-    >
-      <Typography variant="h1" component={"h1"}>
-        Tienda
-      </Typography>
-      <Typography variant="h2" sx={{ mb: 1 }}>
-        Todos los productos
-      </Typography>
-      <ProductList products={initialData.products as any} />
-    </ShopLayout>
-  );
+const HomePage: NextPage = () => {
+	const { products, isLoading } = useProducts('/products');
+
+	return (
+		<ShopLayout
+			title={'Teslo-Shop - Home'}
+			pageDescription={'Encuentra Productos'}
+		>
+			<Typography variant='h1' component={'h1'}>
+				Tienda
+			</Typography>
+			<Typography variant='h2' sx={{ mb: 1 }}>
+				Todos los productos
+			</Typography>
+			{isLoading ? <FullScreenLoading /> : <ProductList products={products} />}
+		</ShopLayout>
+	);
 };
 
-export default Home;
+export default HomePage;
