d0c2b0bd671fa5220a86f718e8152cb3bdfee1fb
riverag8504 <riverag8504@gmail.com>
2022-06-18 01:15:13 -0400

    created ssr for search page

--- pages/search/[query].tsx
+++ pages/search/[query].tsx
@@ -1,16 +1,18 @@
 import { Typography } from '@mui/material';
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
 import { ShopLayout } from '../../components/layouts';
 import { ProductList } from '../../components/products';
 import { useProducts } from '../../hooks';
 import { FullScreenLoading } from '../../components/ui';
 import { useRouter } from 'next/router';
+import { dbProducts } from '../../database';
+import { IProduct } from '../../interfaces';
 
-const SearchPage: NextPage = () => {
-	const { query } = useRouter();
+interface Props {
+	products: IProduct[];
+}
 
-	const { products, isLoading } = useProducts(`/search/${query.query}`);
-
+const SearchPage: NextPage<Props> = ({ products }) => {
 	return (
 		<ShopLayout
 			title={'Teslo-Shop - Search'}
@@ -20,11 +22,33 @@ const SearchPage: NextPage = () => {
 				Buscar producto
 			</Typography>
 			<Typography variant='h2' sx={{ mb: 1 }}>
-				"{query.query}"
+				ABC --- 123
 			</Typography>
-			{isLoading ? <FullScreenLoading /> : <ProductList products={products} />}
+			<ProductList products={products} />
 		</ShopLayout>
 	);
 };
 
 export default SearchPage;
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+	const { query = '' } = params as { query: string };
+
+	if (query?.length === 0) {
+		return {
+			redirect: {
+				destination: '/',
+				permanent: true,
+			},
+		};
+	}
+
+	// TODO: retornar otros productos
+	let products = await dbProducts.getProductsByTerm(query);
+
+	return {
+		props: {
+			products,
+		},
+	};
+};
