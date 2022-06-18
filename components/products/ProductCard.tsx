83c3e77607ae4c6b3bc251781972bcd2932a3178
riverag8504 <riverag8504@gmail.com>
2022-06-17 22:57:33 -0400

    product page with getStaticProps

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -34,7 +34,7 @@ export const ProductCard: FC<Props> = ({ product }) => {
 			onMouseLeave={() => setisHovered(false)}
 		>
 			<Card>
-				<NextLink href={'/product/slug'} passHref prefetch={false}>
+				<NextLink href={`/product/${product?.slug}`} passHref prefetch={false}>
 					<Link>
 						<CardActionArea>
 							<CardMedia
---
+++ pages/product/[slug].tsx
@@ -0,0 +1,87 @@
+import { Box, Button, Chip, Grid, Typography } from '@mui/material';
+import { GetServerSideProps, NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { ShopLayout } from '../../components/layouts';
+import { ProductSlideShow, SizeSelector } from '../../components/products';
+import { ItemCounter } from '../../components/ui';
+import { dbProducts } from '../../database';
+import { useProducts } from '../../hooks';
+import { IProduct } from '../../interfaces';
+
+interface Props {
+	product: IProduct;
+}
+
+const ProductPage: NextPage<Props> = ({ product }) => {
+	// const router = useRouter();
+
+	// const { products: product, isLoading } = useProducts(
+	// 	`/products/${router.query.slug}`
+	// );
+
+	return (
+		<ShopLayout title={product.title} pageDescription={product.description}>
+			<Grid container spacing={3}>
+				<Grid item xs={12} sm={7}>
+					<ProductSlideShow images={product.images} />
+				</Grid>
+				<Grid item xs={12} sm={5}>
+					<Box display={'flex'} flexDirection={'column'}>
+						{/* Titles */}
+						<Typography variant='h1' component={'h1'}>
+							{product.title}
+						</Typography>
+						<Typography variant='subtitle1' component={'h2'}>
+							${product.price}
+						</Typography>
+						{/* Amount */}
+						<Box sx={{ my: 2 }}>
+							<Typography variant='subtitle2'>Cantidad</Typography>
+							<ItemCounter />
+							<SizeSelector
+								selectedSize={product?.sizes[2]}
+								sizes={product?.sizes}
+							/>
+						</Box>
+						{/* Add to cart */}
+						<Button color='secondary' className='circular-btn'>
+							Agregar al carrito
+						</Button>
+
+						{/* <Chip label="No hay disponibles" color="error" variant="outlined" /> */}
+
+						{/* Description */}
+						<Box sx={{ mt: 3 }}>
+							<Typography variant='subtitle2'>Descripción</Typography>
+							<Typography variant='body2'>{product.description}</Typography>
+						</Box>
+					</Box>
+				</Grid>
+			</Grid>
+		</ShopLayout>
+	);
+};
+
+// getServerSideProps
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+	const { slug = '' } = params as { slug: string };
+
+	const product = await dbProducts.getProductBySlug(slug);
+
+	if (!product) {
+		return {
+			redirect: {
+				destination: '/',
+				permanent: false,
+			},
+		};
+	}
+
+	return {
+		props: {
+			product,
+		},
+	};
+};
+
+export default ProductPage;
