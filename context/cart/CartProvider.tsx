a191a3c76c485c06a966b3e8b16a2fa5904847b8
riverag8504 <riverag8504@gmail.com>
2022-06-19 19:48:15 -0400

    updated cart validations

--- context/cart/CartProvider.tsx
+++ context/cart/CartProvider.tsx
@@ -17,10 +17,45 @@ type ProviderProps = {
 export const CartProvider = ({ children }: ProviderProps) => {
 	const [state, dispatch] = useReducer(cartReducer, CART_INITIAL_STATE);
 
+	const addProductToCart = (product: ICartProduct) => {
+		//dispatch({ type: '[Cart] - Add Product', payload: product });
+
+		const productInCart = state.cart.some((p) => p._id === product._id);
+		if (!productInCart)
+			return dispatch({
+				type: '[Cart] - Update products in cart',
+				payload: [...state.cart, product],
+			});
+		const productInCartButDifferentSize = state.cart.some(
+			(p) => p._id === product._id && p.size === product.size
+		);
+
+		if (!productInCartButDifferentSize)
+			return dispatch({
+				type: '[Cart] - Update products in cart',
+				payload: [...state.cart, product],
+			});
+
+		// Acumular
+		const updateProduct = state.cart.map((p) => {
+			if (p._id !== product._id) return p;
+			if (p.size !== product.size) return p;
+
+			p.quantity += product.quantity;
+			return p;
+		});
+
+		dispatch({
+			type: '[Cart] - Update products in cart',
+			payload: updateProduct,
+		});
+	};
+
 	return (
 		<CartContext.Provider
 			value={{
 				...state,
+				addProductToCart,
 			}}
 		>
 			{children}
