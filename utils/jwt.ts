0505a0ba62601a2d7cd06ec331e493894ee782ce
Ernesto Rivera <riverag8504@gmail.com>
2022-07-08 23:54:56 -0400

    [feature] validated token

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -20,3 +20,22 @@ export const signToken = (_id: string, email: string) => {
     },
   );
 };
+
+export const isValidToken = (token: string): Promise<string> => {
+  if (!process.env.JWT_SECRET_SEED) {
+    throw new Error('No JWT seed - Check env variables');
+  }
+
+  return new Promise((resolve, reject) => {
+    try {
+      jwt.verify(token, process.env.JWT_SECRET_SEED || '', (err, payload) => {
+        if (err) return reject('Not valid JWT');
+        const { _id } = payload as { _id: string };
+
+        resolve(_id);
+      });
+    } catch (error) {
+      reject('Not valid JWT');
+    }
+  });
+};
