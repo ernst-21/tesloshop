633d0c4c47fc2cfbc6c7c27f3848052e4d39419d
Ernesto Rivera <riverag8504@gmail.com>
2022-07-09 01:02:01 -0400

    [feature] connected login form to backend

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NextLink from 'next/link';
 import { AuthLayout } from '../../components/layouts';
-import { Box, Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { Box, Button, Chip, Grid, Link, TextField, Typography } from '@mui/material';
 import { useForm } from 'react-hook-form';
 import { validations } from '../../utils';
+import { tesloApi } from '../../api';
+import { ErrorOutlined } from '@mui/icons-material';
 
 type FormData = {
     email: string,
@@ -12,9 +14,21 @@ type FormData = {
 
 const LoginPage = () => {
     const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+    const [showError, setShowError] = useState(false);
 
-    const onLoginUser = (data: FormData) => {
-        return console.log({ data });
+    const onLoginUser = async ({ email, password }: FormData) => {
+        try {
+            const { data } = await tesloApi.post('/user/login', { email, password });
+            const { token, user } = data;
+            console.log({ token, user });
+        } catch (error) {
+            setShowError(true);
+            setTimeout(() => {
+                setShowError(false);
+            }, 3000);
+        }
+
+        // todo: navigate to previous screen or home
     };
 
     return (
@@ -26,6 +40,12 @@ const LoginPage = () => {
                             <Typography variant='h1' component='h1'>
                                 Sign in
                             </Typography>
+                            <Chip className='fadeIn'
+                                  sx={{ display: showError ? 'flex' : 'none', justifyContent: 'flex-start' }}
+                                  label='User o password not found'
+                                  color='error'
+                                  icon={<ErrorOutlined />}
+                            />
                         </Grid>
 
                         <Grid item xs={12}>
@@ -36,7 +56,7 @@ const LoginPage = () => {
                                 fullWidth
                                 {...register('email', {
                                     required: 'This field is required',
-                                    validate: validations.isEmail
+                                    validate: validations.isEmail,
                                 })}
                                 error={!!errors.email}
                                 helperText={errors.email?.message}
@@ -51,7 +71,7 @@ const LoginPage = () => {
                                 fullWidth
                                 {...register('password', {
                                     required: 'This field is required',
-                                    minLength: {value: 6, message: 'Password must contain at least 6 characters'}
+                                    minLength: { value: 6, message: 'Password must contain at least 6 characters' },
                                 })}
                                 error={!!errors.password}
                                 helperText={errors.password?.message}
