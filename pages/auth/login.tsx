d385093caccedab4ec3127f0fecc40f5f80fe635
Ernesto Rivera <riverag8504@gmail.com>
2022-07-09 00:33:19 -0400

    [feature] react-hook-form for login screen

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,51 +1,84 @@
-import { Box, Button, Grid, Link, TextField, Typography } from "@mui/material";
-import NextLink from "next/link";
-import React from "react";
-import { AuthLayout } from "../../components/layouts";
+import React from 'react';
+import NextLink from 'next/link';
+import { AuthLayout } from '../../components/layouts';
+import { Box, Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { useForm } from 'react-hook-form';
+import { validations } from '../../utils';
+
+type FormData = {
+    email: string,
+    password: string,
+};
 
 const LoginPage = () => {
-  return (
-    <AuthLayout title="Ingresar">
-      <Box sx={{ width: 350, pading: "10px 20px" }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Typography variant="h1" component="h1">
-              Iniciar Sesión
-            </Typography>
-          </Grid>
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
 
-          <Grid item xs={12}>
-            <TextField label="Correo" variant="filled" fullWidth />
-          </Grid>
+    const onLoginUser = (data: FormData) => {
+        return console.log({ data });
+    };
 
-          <Grid item xs={12}>
-            <TextField
-              label="Contraseña"
-              type="password"
-              variant="filled"
-              fullWidth
-            />
-          </Grid>
+    return (
+        <AuthLayout title='Ingresar'>
+            <form onSubmit={handleSubmit(onLoginUser)} noValidate>
+                <Box sx={{ width: 350, pading: '10px 20px' }}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <Typography variant='h1' component='h1'>
+                                Sign in
+                            </Typography>
+                        </Grid>
 
-          <Grid item xs={12}>
-            <Button
-              color="secondary"
-              className="circular-btn"
-              size="large"
-              fullWidth
-            >
-              Ingresar
-            </Button>
-          </Grid>
-          <Grid item xs={12} display="flex" justifyContent={"end"}>
-            <NextLink href={"/auth/register"} passHref>
-              <Link underline="always">No tienes cuenta?</Link>
-            </NextLink>
-          </Grid>
-        </Grid>
-      </Box>
-    </AuthLayout>
-  );
+                        <Grid item xs={12}>
+                            <TextField
+                                type='email'
+                                label='Email'
+                                variant='filled'
+                                fullWidth
+                                {...register('email', {
+                                    required: 'This field is required',
+                                    validate: validations.isEmail
+                                })}
+                                error={!!errors.email}
+                                helperText={errors.email?.message}
+                            />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <TextField
+                                label='Password'
+                                type='password'
+                                variant='filled'
+                                fullWidth
+                                {...register('password', {
+                                    required: 'This field is required',
+                                    minLength: {value: 6, message: 'Password must contain at least 6 characters'}
+                                })}
+                                error={!!errors.password}
+                                helperText={errors.password?.message}
+                            />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <Button
+                                type='submit'
+                                color='secondary'
+                                className='circular-btn'
+                                size='large'
+                                fullWidth
+                            >
+                                Login
+                            </Button>
+                        </Grid>
+                        <Grid item xs={12} display='flex' justifyContent={'end'}>
+                            <NextLink href={'/auth/register'} passHref>
+                                <Link underline='always'>Don't have an account yet?</Link>
+                            </NextLink>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </form>
+        </AuthLayout>
+    );
 };
 
 export default LoginPage;
